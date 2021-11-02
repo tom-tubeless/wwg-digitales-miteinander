f9c7f57f67041989cdfb3351bcf242d70b89fc3b
tom_tubeless <lutz.tomala@gmail.com>
2021-11-02 08:49:37 +0100

    added 'Theoretische Grundlagen'

--- book/_build/jupyter_execute/docs/002_theoretische_grundlage.py
+++ book/_build/jupyter_execute/docs/002_theoretische_grundlage.py
@@ -3,4 +3,28 @@
 
 # # Theoretische Grundlage
 # 
-# tttt
+# Der Begriff des _Medialitätsbewusstseins_ (vgl. {cite:ps}`Groeben2002`, S. 166-192) begründet die Forderung nach Medienbildung als einem integralen Bestandteil der Allgemeinbildung.
+# Demnach liefern Medien in der Regel konstruierte und inszenierte Ausschnitte aus der Wirklichkeit.
+# Dadurch haben sie einen konstitutiven und wachsenden Anteil an der Generierung, Speicherung und Übermittlung von Informationen und Wissen.
+# 
+# Grundlage der schulinternen Planungen bildet der [Medienkompetenzrahmen](https://www.schulministerium.nrw.de/docs/bp/Ministerium/Schulverwaltung/Schulmail/Archiv-2018/180626/Kontext/2018_Medienkompetenzrahmen_NRW.pdf) NRW der Landesregierung Nordrhein-Westfalen in der Fassung von März 2018 {cite:ps}`MKR2018`.
+# Der Leitfaden orientiert sich an Dieter Baackes Kompetenzbegriff. {cite:ps}`Baacke1999`
+# Demnach beruht der kompetente Umgang mit Medien auf vier Säulen: _Medienkritik_, _Medienkunde_, _Mediennutzung_ und _Mediengestaltung_.
+# 
+# Die ersten beiden Aspekte fokussieren die Vermittlung von Medienkompetenz:
+# 
+# 1. _Medienkritik_ kombiniert die Analyse von gesellschaftlichen Prozessen in Bezug auf Medien sowie die Reflexion auf das eigene Handeln im Umgang mit Medien (Medien-Ethik). Ein wichtiger Bestandteil dieser Säule ist dem Thema „Verantwortungsvoller Umgang mit Medien“ gewidmet, der unter anderem Cybermobbing und Gefahren im Internet behandelt.
+# 2. _Medienkunde_ umfasst Wissen über historische und aktuelle Mediensysteme. Es wird die informative (reines Wissen) von der instrumentell-qualifikatorische (Fähigkeit zur Bedienung).
+# 
+# Die letzten beiden Aspekte stellen die Handlung in den Mittelpunkt und sind damit zielorientiert:
+# 
+# 3. Bei der _Mediennutzung_ sollen Medien sowohl rezeptiv angewendet, als auch interaktiv angeboten werden.
+# 4. Zum Aspekt der _Mediengestaltung_ gehört die _innovative_ Veränderung und Entwicklung des Mediensystems und die _kreative_ Gestaltung von Medien.
+# 
+# Ausgestaltet sind die Aspekte der allgemeinen Medienkompetenz im Kapitel Curriculum. Sie verzahnen dabei den Medienkompetenz-Rahmen NRW, den Medienpass NRW sowie die schulinternen Curricula der einzelnen Fächer.
+# 
+# Eine besondere Rolle wird in Zukunft die Informatische Grundbildung in der Erprobungsstufe spielen. Neben dem den Lehrplänen des Landes werden soll die inhaltliche Gestaltung des schulinternen Lehrplans den internationalen [CSTA K–12 CS Standards](https://www.csteachers.org/page/about-csta-s-k-12-nbsp-standards) entsprechen {cite:ps}`CSTA2017`.
+# 
+# ```{bibliography}
+# :style: plain
+# ```
